eb62fdb13a4f288e59ca7d8d096625c19173e232
tine <tinesaenen@hotmail.be>
2020-01-03 11:37:16 +0100

    jquery mechanisme voor het filteren van de watches werkt!!!

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -36,17 +36,63 @@
 
 // probeersel 3
 
-$("select.filterby").change(function() {
-  var filters = $.map($("select.filterby").toArray(), function(e) {
-    return $(e).val();
-  }).join(".");
-  $("div#FilterContainer")
-    .find("div")
-    .hide();
-  $("div#FilterContainer")
-    .find("div." + filters)
-    .show();
+// $("select.filterby").change(function() {
+//   var filters = $.map($("select.filterby").toArray(), function(e) {
+//     return $(e).val();
+//   }).join(".");
+//   $("div#FilterContainer")
+//     .find("div")
+//     .hide();
+//   $("div#FilterContainer")
+//     .find("div." + filters)
+//     .show();
+// });
+
+// probeersel 4
+
+// $("#filer-1 li").click(function() {
+//   $(".filters-filer-1 .filtr").removeClass("filtr-active");
+//   $(this).addClass("filtr-active");
+//   var filter = $(this).data("fltr");
+//   filer1.filterizr("filter", filter);
+// });
+
+//probeersel 5
+var genderSelector = "";
+var propertySelector = "";
+
+var filter = function() {
+  var selector = propertySelector + genderSelector;
+
+  if (selector) {
+    $(".item").hide();
+    $(propertySelector + genderSelector).show();
+  } else {
+    $(".item").show();
+  }
+};
+
+$("#gender input").change(function() {
+  genderSelector =
+    this.value == "men" ? ".men:not(.women)" : ".women:not(.men)";
+  filter();
 });
+
+$("#property input").change(function() {
+  if ($(this).is(":checked")) {
+    propertySelector = propertySelector + "." + this.id;
+  } else {
+    propertySelector = propertySelector.replace("." + this.id, "");
+  }
+  filter();
+});
+
+// this runs on load so the initial view will be pdated
+genderSelector =
+  $("#gender :checked").val() == "men"
+    ? ".men:not(.women)"
+    : ".women:not(.men)";
+filter();
 
 /*!
  * jQuery JavaScript Library v3.0.0
